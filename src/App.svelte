cccaa33c2c8bf3dfb17dfbf3abe619043f327b9f
Marco Ceriani <marco.ceriani@eidosmedia.com>
2023-07-05 18:06:54 +0200

    replacing sveltekit with plain vite

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+</script>
+
+<div class="root-container">
+	<header>
+		<h1>Color Water Tubes</h1>
+	</header>
+
+	<main>
+		
+	</main>
+</div>
+
+<style>
+	header {
+		text-align: center;
+		font-size: clamp(1rem, 3.5vw, 1.25rem);
+		margin-block: 0.75rem;
+	}
+	h1 {
+		background: linear-gradient(180deg, var(--clr-accent) 50%, var(--clr-accent2) 75%);
+        background-clip: text;
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: transparent;
+	}
+	.root-container {
+		max-width: 50rem;
+		height: 100%;
+		display: grid;
+		grid-template-rows: min-content 1fr min-content;
+		margin-inline: auto;
+		padding-inline: 1rem;
+	}
+</style>
